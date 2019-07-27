2af93bb85b4dc2e7636e98db0083322bc040d666
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2019-07-27 11:37:12 +0300

    Add weather data
    Add weather data and other data to data-raw folder, and scripts to copy and compress files in data folder.

---
+++ data-raw/select-and-save-weather-data.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(lubridate)
+library(wrapr)
+
+load("./data-raw/weather-data/weather-June-2019-tb.rda")
+
+weather_June_2019.tb %.>%
+  filter(., week(time) == 24L) -> weather_wk_25_2019.tb
+
+save(weather_wk_25_2019.tb, file = "./data/weather-wk-25-2019-tb.rda", compress = "xz", compression_level = 3)
